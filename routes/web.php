2a96f6764cd5e70c6c7f825e35ddfad01cc5d255
Fixron Maulana <fikronmaulana27@gmail.com>
2025-06-24 16:38:31 +0700

    make route and controller admin services

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\Admin\AboutController;
 use App\Http\Controllers\Admin\MediaController;
+use App\Http\Controllers\Admin\ServiceController;
 use App\Http\Controllers\Admin\DashboardController;
 
 /*Route::get('/', function () {
@@ -19,6 +20,8 @@ Route::patch('/admin/abouts', [AboutController::class, 'update'])->name('abouts.
 Route::get('/admin/medias', [MediaController::class, 'index'])->name('medias.index');
 Route::post('/admin/medias', [MediaController::class, 'store'])->name('medias.store');
 Route::delete('/admin/medias/{media}', [MediaController::class, 'destroy'])->name('medias.destroy');
+// Services routes
+Route::get('/admin/services', [ServiceController::class, 'index'])->name('admin.service.index');
 
 Route::get('/{any}', function () {
     return view('notFoundPage');
